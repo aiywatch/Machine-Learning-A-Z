1173f249d7b0d9bd897a67bbe2755a2e3f30c3e7
aiywatch <aiycp35@gmail.com>
2017-06-11 05:38:43 +0700

    Decision tree

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -15,12 +15,11 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
 
 # Fitting the SVR Model to the dataset
 from sklearn.svm import SVR
@@ -28,7 +27,7 @@ regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
 
 # Visualising the SVR results
 plt.scatter(X, y, color = 'red')
